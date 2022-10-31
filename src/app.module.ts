c09cea328445e0469c3dd8b4eab17c32e6b4ef2c
pino._.hoo <inhoo23@naver.com>
2022-10-31 17:11:42 +0900

    add : config Module Set

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config/dist/config.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
