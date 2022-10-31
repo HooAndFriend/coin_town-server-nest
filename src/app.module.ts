59ca8a29169353db47956bc82490eaa4f7ccbff8
pino._.hoo <inhoo23@naver.com>
2022-10-31 17:13:12 +0900

    add : Typeorm Set

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config/dist/config.module'
+import { AuthModule } from './auth/auth.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   imports: [
@@ -7,6 +9,17 @@ import { ConfigModule } from '@nestjs/config/dist/config.module'
       isGlobal: true,
       envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
     }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ['dist/**/*.entity.js'],
+      synchronize: true,
+    }),
+    AuthModule,
   ],
   controllers: [],
   providers: [],
