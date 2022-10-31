87fe4db8592fdd67e8063938f80548b71469bfc3
pino._.hoo <inhoo23@naver.com>
2022-10-31 17:15:55 +0900

    add : main Basic Set

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  })
+  app.useGlobalPipes(new ValidationPipe())
+
+  await app.listen(process.env.NODE_SERVER_PORT)
 }
 bootstrap()
