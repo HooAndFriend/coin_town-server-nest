c6e9be0a147b361f2660bfdc811c2fd047a0990c
pino._.hoo <inhoo23@naver.com>
2022-11-15 00:34:01 +0900

    add : Local Save API

--- src/auth/ui/auth.controller.ts
+++ src/auth/ui/auth.controller.ts
@@ -1,17 +1,24 @@
-import { Controller, Post } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { ApiResponse } from 'src/common/response/reponse.dto'
 import { AuthService } from '../application/auth.service'
+import { LocalSaveDto } from '../dto/user.save.dto'
+import { LocalGuard } from '../passport/passport.local.guard'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post("/local")
-  async localLogin(){
+  @Post('/local')
+  @UseGuards(LocalGuard)
+  async localLogin() {}
 
-  }
-
-  @Post("/")
-  async localSave(){
-    
+  @Post('/')
+  async localSave(@Body() body: LocalSaveDto) {
+    const response = await this.authService.localSave(body)
+    return ApiResponse.of({
+      data: response,
+      message: 'Success Find Book',
+      statusCode: 200,
+    })
   }
 }
