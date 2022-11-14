213690472cff281c5126b441f758f8ad54282085
pino._.hoo <inhoo23@naver.com>
2022-11-15 00:44:27 +0900

    add : Local Login API COntroller

--- src/auth/ui/auth.controller.ts
+++ src/auth/ui/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common'
 import { ApiResponse } from 'src/common/response/reponse.dto'
 import { AuthService } from '../application/auth.service'
 import { LocalSaveDto } from '../dto/user.save.dto'
@@ -10,14 +10,22 @@ export class AuthController {
 
   @Post('/local')
   @UseGuards(LocalGuard)
-  async localLogin() {}
+  async localLogin(@Req() req) {
+    const { user } = req
+    const token = await this.authService.gwtJwtWithIdx(user.idx)
+    return ApiResponse.of({
+      data: { user, token },
+      message: 'Success Local Login',
+      statusCode: 200,
+    })
+  }
 
   @Post('/')
   async localSave(@Body() body: LocalSaveDto) {
     const response = await this.authService.localSave(body)
     return ApiResponse.of({
       data: response,
-      message: 'Success Find Book',
+      message: 'Success Save Local User',
       statusCode: 200,
     })
   }
